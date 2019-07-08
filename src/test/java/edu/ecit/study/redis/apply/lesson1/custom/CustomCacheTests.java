9c4083128939797f9bf3fe2128a4cada9fe984bc
1098298479 <1098298479@qq.com>
2019-07-08 23:38:14 +0800

    关于缓存的学习，StringRedisTemplate、RedisTemplate、Spring Cache的注解开发。

---
+++ src/test/java/edu/ecit/study/redis/apply/lesson1/custom/CustomCacheTests.java
@@ -0,0 +1,26 @@
+package edu.ecit.study.redis.apply.lesson1.custom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import edu.ecit.study.redis.apply.pojo.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+@ActiveProfiles("custom") // 设置profile
+public class CustomCacheTests {
+
+    @Autowired
+    CustomAnnoDemoService customDemoService;
+
+    // get
+    @Test
+    public void springCacheTest() throws Exception {
+        User user = customDemoService.findUserById("wahaha");
+        System.out.println(user);
+    }
+}
